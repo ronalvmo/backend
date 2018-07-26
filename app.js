c5876ad83e14dddc84687df22381878758f47246
Ronny Alvarado <ronalvmo@hotamail.com>
2018-07-25 21:38:29 -0600

    commit

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 //requires cargar librerias propias o de terceros
 var express = require('express');
+var mongoose = require('mongoose');
 
 //inicializar variables
 
 var app = express();
+
+//conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDb', (error, res) => {
+    if (error) throw err;
+    console.log('Base de datos \x1b[32m%s\x1b[0m', 'online');
+
+});
 
 //rutas
 app.get('/', (req, res, next) => {
