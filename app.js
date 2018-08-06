0d973a5d7f9d68ee96506b824bc004e16207bb78
Ronny Alvarado <ronalvmo@hotamail.com>
2018-08-05 21:48:21 -0600

    coomi

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var mongoose = require('mongoose');
 
 var app = express();
 
+//cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+  });
+
 //importar rutas
 
 var appRoutes = require('./routes/app');
